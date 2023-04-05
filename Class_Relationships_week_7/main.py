5204e3018ca5a567ac2136e2370e9ed15fa040ef
MrPsycho <slurpylord@gmail.com>
2023-04-05 16:53:29 +1200

    Write a Python function to return the list of words that are longer than n from a given list of words Used list comprehension we just say for each word in the list if the len of that word is greater than the number given and return then list of words

--- Class_Relationships_week_7/main.py
+++ Class_Relationships_week_7/main.py
@@ -1,8 +1,6 @@
 
 
 # Write a Python function that takes two lists and returns True if they have at least one common member
-
-
 def common_member(listA, listB):
     if len(set(listA).intersection(listB)) >= 1:
         return True
@@ -16,3 +14,13 @@ print(common_member([1, 2, 3, 4], [4, 5, 6, 7, 8]))
 print(common_member([1, 2, 3, 4], [3, 4, 5, 6, 7]))
 # false because there are not common members
 print(common_member([1, 2, 3, 4], [5, 6, 7, 8, 9]))
+
+
+# Write a Python function to return the list of words that are longer than n from a given list of words
+def filterlongword(list, number):
+    return [word for word in list if len(word) > number]
+
+
+print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 4))
+print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 3))
+print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 2))
