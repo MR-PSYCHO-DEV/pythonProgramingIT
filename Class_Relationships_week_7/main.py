3b3a0fa43a6d41f3a2b5fc7ae4f1776eec28f40b
MrPsycho <slurpylord@gmail.com>
2023-04-05 16:32:12 +1200

    Write a Python function that takes two lists and returns True if they have at least one common member.
    I convernted the listA into a set then I using intersection that returns the common members.
    Then I used len to get the lenth and checked if there is more than 1 in that set
    returns true if there is more than one | else false

---
+++ Class_Relationships_week_7/main.py
@@ -0,0 +1,18 @@
+
+
+# Write a Python function that takes two lists and returns True if they have at least one common member
+
+
+def common_member(listA, listB):
+    if len(set(listA).intersection(listB)) >= 1:
+        return True
+    else:
+        return False
+
+
+# true because there is 4 in the in both
+print(common_member([1, 2, 3, 4], [4, 5, 6, 7, 8]))
+# true because there is 3 and 4 in the in both
+print(common_member([1, 2, 3, 4], [3, 4, 5, 6, 7]))
+# false because there are not common members
+print(common_member([1, 2, 3, 4], [5, 6, 7, 8, 9]))
