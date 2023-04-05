675ab88b1f92d9dc89695fea54ff8ab108192d64
MrPsycho <slurpylord@gmail.com>
2023-04-05 17:31:43 +1200

    Write a Python program to shuffle and print a stored list imported shuffle

--- Class_Relationships_week_7/common_member.py
+++ Class_Relationships_week_7/common_member.py
@@ -14,13 +14,3 @@ print(common_member([1, 2, 3, 4], [4, 5, 6, 7, 8]))
 print(common_member([1, 2, 3, 4], [3, 4, 5, 6, 7]))
 # false because there are not common members
 print(common_member([1, 2, 3, 4], [5, 6, 7, 8, 9]))
-
-
-# Write a Python function to return the list of words that are longer than n from a given list of words
-def filterlongword(list, number):
-    return [word for word in list if len(word) > number]
-
-
-print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 4))
-print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 3))
-print(filterlongword("The quick brown fox jumps over the lazy dog".split(), 2))
